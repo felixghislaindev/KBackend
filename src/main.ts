1eb8c5ca5ff58c86c42bb41e2be98f1911a27ddf
felixghislaindev <felixghislain@yahoo.com>
2025-01-15 21:48:35 +0000

    setting up cors

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,7 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // adding cors
-  const allowedOrigins =
-    process.env.NODE_ENV === 'production'
-      ? ['https://kfrontend-production.up.railway.app/']
-      : ['http://localhost:3000'];
-
-  app.enableCors({
-    origin: allowedOrigins,
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT || 8000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
