7fa92fa5687bab4e592f2b08924099eedc16ab09
felixghislaindev <felixghislain@yahoo.com>
2025-01-15 21:18:48 +0000

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // adding cors
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow only localhost:3000
+    methods: 'GET, POST, PUT, DELETE', // Specify allowed methods
+    allowedHeaders: 'Content-Type, Accept', // Specify allowed headers
+  });
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
