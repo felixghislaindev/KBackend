897770925efe630081d751a4836a69bb40a0811b
felixghislaindev <felixghislain@yahoo.com>
2025-01-16 13:07:52 +0000

    setting up default route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,10 @@ import { AppService, DeliveryDetails } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/')
+  welcomeToNextDelivery(): string {
+    return 'Hello! Welcome to your next delivery service. Please provide your user ID to get the delivery details.';
+  }
   @Get('/your-next-delivery/:id')
   getNextDelivery(
     @Param('id') userId: string,
